08d7ee35435d81ae38702cb94a6736d380a8d124
Jose Cerqueira <josecerqueira@protonmail.com>
2025-11-21 11:18:57 +0000

    :fire: chore: remove uneeded doce

--- rust-circuit/src/lib.rs
+++ rust-circuit/src/lib.rs
@@ -4,8 +4,5 @@ pub mod merkle_tree;
 pub mod poseidon_opt;
 pub mod utils;
 
-#[cfg(target_arch = "wasm32")]
-pub mod wasm;
-
 pub const ZERO_VALUE: &str =
     "18688842432741139442778047327644092677418528270738216181718229581494125774932";
